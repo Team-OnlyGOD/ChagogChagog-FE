88f5b92452396e1003bd0cd3959f6fde772a7041
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-10-12 19:40:52 +0900

    충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { OverlayProvider } from "@toss/use-overlay";
 import Router from "./components/Router";
 import Provider from "./components/common/Provider";
 
 function App() {
   return (
-    <OverlayProvider>
-      <Provider>
-        <Router />
-      </Provider>
-    </OverlayProvider>
+    <Provider>
+      <Router />
+    </Provider>
   );
 }
 
