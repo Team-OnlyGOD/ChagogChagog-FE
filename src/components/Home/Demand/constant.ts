c133e552310c7b22a977e4ee71b3250a45eda5e2
SeoJumee <official.jumee@gmail.com>
2023-10-13 01:22:54 +0900

    Feat :: 수요량별 재고 내역 기능

--- src/components/Home/Demand/constant.ts
+++ src/components/Home/Demand/constant.ts
@@ -1,5 +1,10 @@
-export const SELLING_TYPE = [
-  "주문이 가장 많은 상품",
-  "지속적인 주문이 있는 상품",
-  "주문이 가장 적은 상품",
+interface SellingType {
+  label: string;
+  value: "a" | "b" | "c";
+}
+
+export const SELLING_TYPE: SellingType[] = [
+  { label: "주문이 가장 많은 상품", value: "a" },
+  { label: "지속적인 주문이 있는 상품", value: "b" },
+  { label: "주문이 가장 적은 상품", value: "c" },
 ];
