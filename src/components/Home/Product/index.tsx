fb77f9e75cd1f0ff372a7fd7819a941dc0c49ee9
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-10-12 15:53:21 +0900

    Feat :: 재고현황 테이블 퍼블리싱

--- src/components/Home/Product/index.tsx
+++ src/components/Home/Product/index.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import {
+  CheckBoxTH,
+  MiddleTH,
+  Flex,
+  BigBoxTH,
+  WhiteBigBoxTH,
+  WhiteMiddleTH,
+  ButtonWrap,
+  SmallButton,
+} from "./style";
+import CheckBox from "../../common/CheckBox";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <>
+      <ButtonWrap>
+        <SmallButton>판매중지</SmallButton>
+        <SmallButton>판매재개</SmallButton>
+      </ButtonWrap>
+      <div style={{ marginTop: "24px" }}>
+        <Flex>
+          <CheckBoxTH>
+            <CheckBox />
+          </CheckBoxTH>
+          <MiddleTH>판매상태</MiddleTH>
+          <BigBoxTH>출고상품명</BigBoxTH>
+          <MiddleTH>총 재고</MiddleTH>
+          <MiddleTH>안전 재고</MiddleTH>
+          <MiddleTH>정상 재고</MiddleTH>
+          <MiddleTH>분량 재고</MiddleTH>
+          <MiddleTH>제조일자</MiddleTH>
+        </Flex>
+        {Array.from({ length: 6 }).map(() => (
+          <Flex>
+            <CheckBoxTH>
+              <CheckBox />
+            </CheckBoxTH>
+            <WhiteMiddleTH>판매상태</WhiteMiddleTH>
+            <WhiteBigBoxTH>출고상품명</WhiteBigBoxTH>
+            <WhiteMiddleTH>총 재고</WhiteMiddleTH>
+            <WhiteMiddleTH>안전 재고</WhiteMiddleTH>
+            <WhiteMiddleTH>정상 재고</WhiteMiddleTH>
+            <WhiteMiddleTH>분량 재고</WhiteMiddleTH>
+            <WhiteMiddleTH>제조일자</WhiteMiddleTH>
+          </Flex>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Product;
---
+++ src/components/Home/Product/style.ts
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+export const Flex = styled.div`
+  display: flex;
+`;
+
+export const MiddleTH = styled.div`
+  width: 120px;
+  height: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.Gray50};
+  border-right: 1px solid ${({ theme }) => theme.Gray300};
+`;
+
+export const WhiteMiddleTH = styled.div`
+  width: 120px;
+  height: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #fff;
+  border-right: 1px solid ${({ theme }) => theme.Gray300};
+`;
+
+export const CheckBoxTH = styled.div`
+  width: 60px;
+  height: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.Gray50};
+  border-right: 1px solid ${({ theme }) => theme.Gray300};
+`;
+
+export const BigBoxTH = styled.div`
+  width: 320px;
+
+  height: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right: 1px solid ${({ theme }) => theme.Gray300};
+
+  background-color: ${({ theme }) => theme.Gray50};
+`;
+
+export const WhiteBigBoxTH = styled.div`
+  width: 320px;
+
+  height: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right: 1px solid ${({ theme }) => theme.Gray300};
+
+  background-color: #fff;
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+
+  column-gap: 8px;
+`;
+
+export const SmallButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 81px;
+  height: 36px;
+
+  border-radius: 8px;
+  border: 1px solid #eef0f2;
+  background: #fff;
+
+  margin-top: 24px;
+`;
